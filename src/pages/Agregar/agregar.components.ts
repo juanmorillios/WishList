de065f430d2dafaf5b7369d5c703bedce294866f
Juan Morillo <juanmorillios@gmail.com>
2019-01-07 20:31:26 +0100

    Add funcionality scren-list

--- src/pages/Agregar/agregar.components.ts
+++ src/pages/Agregar/agregar.components.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WishlistService } from '../../app/services/wishlist.services';
 import { List } from '../../app/models/list.model';
 import { NavParams } from 'ionic-angular';
+import { ListItem } from '../../app/models/list-item.model';
 
 @Component({
     selector: 'page-agregar',
@@ -10,12 +11,24 @@ import { NavParams } from 'ionic-angular';
 export class AgregarPage{
 
   lista: List;
+  nombreItem: string = '';
 
   constructor( public wishlistService: WishlistService, 
                private navParams: NavParams){
+                   
       const titulo = this.navParams.get('titulo');
       this.lista = new List( titulo );
-
   }
+   agregarItem() {
+       if ( this.nombreItem.length === 0 ) {
+           return;
+       }
+    const nuevoItem = new ListItem(this.nombreItem);
+    this.lista.items.push( nuevoItem );
+    this.nombreItem = '';
+   }
 
+   actualizarTarea(item: ListItem) {
+    item.completado = !item.completado;
+   }
 }
